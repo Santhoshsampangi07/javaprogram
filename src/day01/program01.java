6d2986a70d3780176110d754b361ae79813d8f7f
Santhoshsampangi07 <122223100+Santhoshsampangi07@users.noreply.github.com>
2023-01-12 12:47:11 +0530

    day02
    Program01

--- src/day01/program01.java
+++ src/day01/program01.java
@@ -1,6 +1,6 @@
 package day01;
 
-public class program01 {
+public class Program01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/day02/Program01.java
@@ -0,0 +1,24 @@
+package day02;
+
+public class Program01 {
+	byte a=-128;
+	short b= 32767;
+	int c=1234567;
+	long d=67890898l;
+	boolean x=true;
+	char y='1';
+	float ab=0.1f;
+	double bc=0.1d;
+
+	public static void main(String[] args) {
+		
+		System.out.println("banglore");
+	
+
+	}
+	
+	
+	
+	
+
+}
