fb4ea2d35eef2b70a920958112317ab00e5c1f1e
Santhoshsampangi07 <122223100+Santhoshsampangi07@users.noreply.github.com>
2023-01-12 12:32:45 +0530

    day01
    program01

--- src/day01/program01.java
+++ src/day01/program01.java
@@ -5,6 +5,7 @@ public class program01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi sandy");
+		System.out.println("hi kumara");
 
 	}
 
